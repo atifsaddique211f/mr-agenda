dd31da2c8f6a2725d72caf39873ef96b0abd6ca6
Atif Saddique <atif.saddique4@gmail.com>
2023-05-08 23:31:56 +0500

    Add swagger support

---
+++ src/util/swagger.util.ts
@@ -0,0 +1,16 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from 'src/app.module';
+
+export function setupSwagger(app: INestApplication) {
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('ModaResa Agenda')
+    .setDescription('ModaResa agenda management')
+    .setVersion('1.0')
+    .build();
+
+  const apiDocument = SwaggerModule.createDocument(app, swaggerConfig, {
+    include: [AppModule],
+  });
+  SwaggerModule.setup('api', app, apiDocument);
+}
